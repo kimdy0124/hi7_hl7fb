e2c9547a6e25cef519b8fb7eeef6d173d9f02f6f
DONGYUN KIM <kimdy960124@gmail.com>
2024-03-20 13:20:08 -0600

    another testing
    firebase.py default setting

---
+++ database/firebase.py
@@ -0,0 +1,13 @@
+import pyrebase
+
+config = {
+  "apiKey": "AIzaSyA4Krn4cuEumQHVVS1oRxaaAkHm2h7Suw8",
+  "authDomain": "hi7-hl7.firebaseapp.com",
+  "databaseURL": "https://hi7-hl7-default-rtdb.firebaseio.com",
+  "projectId": "hi7-hl7",
+  "database": "https://hi7-hl7-default-rtdb.firebaseio.com/",
+  "storageBucket": "hi7-hl7.appspot.com",
+  "messagingSenderId": "410837804656",
+  "appId": "1:410837804656:web:38fe4aeaccb29d1eee36b5",
+  "measurementId": "G-WK8LCJP128"
+};
