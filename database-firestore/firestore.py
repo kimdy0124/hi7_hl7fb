fc5d2bfd874c19ef9c1316013a8419d67352bfe4
DONGYUN KIM <kimdy960124@gmail.com>
2024-03-22 10:54:14 -0600

    methods adding

--- database-firestore/firestore.py
+++ database-firestore/firestore.py
@@ -1,5 +1,17 @@
 import firebase_admin
 from firebase_admin import credentials
-
+from firebase_admin import firestore
 cred = credentials.Certificate("path/to/serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+data = {
+    'TODOLIST': 'Finish your code',
+    'Status': 'Done'
+}
+
+doc_ref = db.collection('taskCollection').document()
+doc_ref.set(data)
+
+print('Document ID:', doc_ref.id)
